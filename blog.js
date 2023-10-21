0018574bef890c92e26411308d68884f0bb1ab38
jlearns9 <142127358+jlearns9@users.noreply.github.com>
2023-10-20 21:16:23 -0500

    uPdate

--- blog.js
+++ blog.js
@@ -1,5 +1,5 @@
 export const aboutMe = {
-    date: "Oct 19, 2023",
+    date: "OCT 19, 2023",
     image: "images/my-new-img.png",
     title: "My new journey as a bootcamp student.",
     text: "After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers."
@@ -14,7 +14,7 @@ export const articles = [
     },
     {
         date: "Oct 19, 2023",
-        image: "images/blog-two-img.png",
+        image: "images/blog-two-img-2.png",
         title: "Blog Two",
         text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     },
@@ -38,7 +38,7 @@ export const articles = [
     },
     {
         date: "Oct 19, 2023",
-        image: "images/blog-six-img.png",
+        image: "images/blog-six-img-2.png",
         title: "Blog Six",
         text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
     },
--- index.js
+++ index.js
@@ -4,24 +4,25 @@ import { aboutMe } from "./blog.js";
 const blogLayout = document.getElementById('blog-layout')
 const aboutMeLayout = document.getElementById('about-me-layout')
 
-articles.forEach((article, index) => {
-    blogLayout.innerHTML += `
-        <div class='article-card'>
-            <img class='article-img' src=${article.image}>
-            <h2>${article.title}</h2>
-            <p>${article.text}<p>
-        </div>
-        `
-})
-
 function aboutMeDisplay() {
     aboutMeLayout.innerHTML = `
-    <div class='about-me-info'>
-        <h3>${aboutMe.date}</h3>
+    <div>
+        <h5>${aboutMe.date}</h5>
         <h1>${aboutMe.title}</h1>
-        <p>${aboutMe.text}</p>
+        <h4>${aboutMe.text}</h4>
     </div>
     `
 }
 
 aboutMeDisplay()
+
+articles.forEach((article, index) => {
+    blogLayout.innerHTML += `
+        <div class='article-card'>
+            <img src=${article.image}>
+            <h5>${article.date}<h5>
+            <h2>${article.title}</h2>
+            <p>${article.text}<p>
+        </div>
+        `
+})
